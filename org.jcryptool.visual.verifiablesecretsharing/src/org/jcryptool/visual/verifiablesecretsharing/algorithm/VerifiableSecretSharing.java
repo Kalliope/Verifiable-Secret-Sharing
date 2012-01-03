3bd88a440c1d727106ce18cef11ddabc82d5295c
Stefan Tiefenbacher <s0910239033@students.fh-hagenberg.at>
2012-01-03 12:34:32 +0100

    algo2

--- org.jcryptool.visual.verifiablesecretsharing/src/org/jcryptool/visual/verifiablesecretsharing/algorithm/VerifiableSecretSharing.java
+++ org.jcryptool.visual.verifiablesecretsharing/src/org/jcryptool/visual/verifiablesecretsharing/algorithm/VerifiableSecretSharing.java
@@ -1,7 +1,9 @@
 package org.jcryptool.visual.verifiablesecretsharing.algorithm;
 
 public class VerifiableSecretSharing {
-	public int[] shares;
+	private int[] shares;
+	private int[] commitments;
+	
 	private int calculatePolynom(int[] coefficients, int x, int p){
 		int s = coefficients[0];
 		int y = s;
@@ -14,14 +16,48 @@ public class VerifiableSecretSharing {
 	}
 	
 	
-	public void calculateShares(int[] coefficients, int p, int n){
+	private void calculateShares(int[] coefficients, int p, int n){
 		int[] shares = new int[n];
 		for(int i=0; i < coefficients.length; i++){
 			shares[i] = calculatePolynom(coefficients, i, p);
 		}
-		this.shares = shares;
+		setShares(shares);
 	}
 	
+	private void commitment(int g, int[] coefficients, int p){
+		
+		int[] commitments = new int[coefficients.length];
+		
+		for(int i=0; i<coefficients.length; i++){
+			commitments[i] = (power(g,coefficients[i])) % p;
+		}
+		
+		setCommitments(commitments);
+	}
+	
+	
+	
+	
+	public int[] getShares() {
+		return shares;
+	}
+
+
+	public void setShares(int[] shares) {
+		this.shares = shares;
+	}
+
+
+	public int[] getCommitments() {
+		return commitments;
+	}
+
+
+	public void setCommitments(int[] commitments) {
+		this.commitments = commitments;
+	}
+
+
 	public int power(double x, double j){
 		return (int)Math.pow(x,j);
 	}
