0163190482a6c2a8d518c1a4084728aaa1fe9a93
Stefan Tiefenbacher <s0910239033@students.fh-hagenberg.at>
2012-01-03 14:28:41 +0100

    algo42

--- org.jcryptool.visual.verifiablesecretsharing/src/org/jcryptool/visual/verifiablesecretsharing/algorithm/VerifiableSecretSharing.java
+++ org.jcryptool.visual.verifiablesecretsharing/src/org/jcryptool/visual/verifiablesecretsharing/algorithm/VerifiableSecretSharing.java
@@ -1,5 +1,11 @@
 package org.jcryptool.visual.verifiablesecretsharing.algorithm;
 
+/**
+ * Implements the algorithms for the Verifiable Secret Sharing.
+ * 
+ * @author Dulghier Christoph, Reisinger Kerstin, Tiefenbacher Stefan, Wagner Thomas
+ *
+ */
 public class VerifiableSecretSharing {
 	
 	/*claculated shares*/
@@ -12,8 +18,8 @@ public class VerifiableSecretSharing {
 	/**
 	 * Calculates the function f(x) = s + ax + bx^2 + ... + ix^i
 	 * y is the result for the chosen x. 
-	 * @param coefficients --> a,b,...,i
-	 * @param x --> Player Id
+	 * @param coefficients --> a, b, ..., i
+	 * @param x --> player Id
 	 * @param p --> prime
 	 * @return y
 	 */
@@ -28,15 +34,25 @@ public class VerifiableSecretSharing {
 		return y;
 	}
 	
-	
+	/**
+	 * Calculates the shares with and without modulo p.
+	 * 
+	 * @param coefficients --> a, b, ..., i
+	 * @param p --> prime
+	 * @param n --> number of player
+	 * @return allShares --> contains one array with the shares 
+	 * and one array with the shares modulo p
+	 */
 	public int[][] calculateShares(int[] coefficients, int p, int n){
 		int[] shares = new int[n];
 		int[] sharesModP = new int[n];
 		int[][] allShares = new int[2][n];
+		
 		for(int i=0; i < n; i++){
 			shares[i] = calculatePolynom(coefficients, i, p);
 			sharesModP[i] = (calculatePolynom(coefficients, i, p)) % p;
 		}
+		
 		setShares(shares);
 		setSharesModP(sharesModP);
 		allShares[0] = shares;
@@ -44,6 +60,14 @@ public class VerifiableSecretSharing {
 		return allShares;
 	}
 	
+	/**
+	 * Calculates the commitments for the coefficients
+	 * (including the secret s).
+	 * @param g --> primitive root
+	 * @param coefficients --> a, b, ..., i
+	 * @param p --> prime
+	 * @return commitments
+	 */
 	public int[] commitment(int g, int[] coefficients, int p){
 		
 		int[] commitments = new int[coefficients.length];
@@ -55,6 +79,13 @@ public class VerifiableSecretSharing {
 		return commitments;
 	}
 	
+	/**
+	 * Checks if the share is correct.
+	 * @param g --> primitive root
+	 * @param p --> prime
+	 * @param playerId --> player Id
+	 * @return true --> check OK; false --> check not OK;
+	 */
 	public boolean check(int g, int p, int playerId){
 		int[] commitments = getCommitments();
 		int[] sharesModP = getSharesModP();
