6ab9b802f408dd140c79d97d00fd6c1e18132161
Kalliope <s1010239025@students.fh-hagenberg.at>
2012-01-18 18:07:36 +0100

    sysout deleted

--- org.jcryptool.visual.verifiablesecretsharing/src/org/jcryptool/visual/verifiablesecretsharing/views/ReconstructionChartComposite.java
+++ org.jcryptool.visual.verifiablesecretsharing/src/org/jcryptool/visual/verifiablesecretsharing/views/ReconstructionChartComposite.java
@@ -152,7 +152,6 @@ public class ReconstructionChartComposite extends Composite {
 				y = y.add(coef[j].multiply(new BigInteger(i + "").pow(j)));
 				// y += coef[j]*Math.pow(i, j);
 			}
-			System.out.println(y.toString());
 			reconstructionSeries.add(i, y);
 		}
 
